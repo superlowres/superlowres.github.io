2f24f14457a3a9fc42a6fbfefe6db0b1bfc00b92
Mélanie <melanief0968@yahoo.fr>
2020-05-15 08:47:59 +0200

    nv_ex_7.5

--- cahier/melanie/ex_7.5/main.js
+++ cahier/melanie/ex_7.5/main.js
@@ -1,197 +1,309 @@
 /**
  * 	Reduction d'une image
  */
-const TRIANGLE_W =  15
+//const TRIANGLE_W =  15
 
 
-const data = [
-	{image: "imgs_0.jpg",
-	mot: "Food",
-	mot2:"Landscape",
-	reponse: 0},
-	{image: "imgs_1.jpg",
-	mot: "Food",
-	mot2:"Landscape",
-	reponse: 2},
-	{image: "imgs_2.jpg",
-	mot: "Food",
-	mot2:"Landscape",
-	reponse: 2},
-	{image: "imgs_3.jpg",
-	mot: "Food",
-	mot2:"Landscape",
-	reponse: 1},
-	{image: "imgs_4.jpg",
-	mot: "C",
-	mot2:"D",
-	reponse: 2},
-	{image: "imgs_5.jpg",
-	mot: "E",
-	mot2:"F",
-	reponse: 1},
-	{image: "imgs_6.jpg",
-	mot: "G",
-	mot2:"H",
-	reponse: 1},
+const data = [{
+    image: "imgs_0.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 1
+  },
+  {
+    image: "imgs_1.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 2
+  },
+  {
+    image: "imgs_2.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 1
+  },
+  {
+    image: "imgs_3.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 1
+  },
+  {
+    image: "imgs_4.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 1
+  },
+  {
+    image: "imgs_5.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 2
+  },
+  {
+    image: "imgs_6.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 1
+  },
+  {
+    image: "imgs_7.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 2
+  },
+  {
+    image: "imgs_8.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 2
+  },
+  {
+    image: "imgs_9.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 2
+  },
+  {
+    image: "imgs_10.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 1
+  },
+  {
+    image: "imgs_11.jpg",
+    mot: "FOOD",
+    mot2: "PET",
+    reponse: 2
+  },
 ]
 let score = 0
 let answer = 0
 let level = 0
 
+
+let num_click = 0
+
+
+
+let fail_sound
+let tada_sound
+
 const NUM_X = 64
 const NUM_Y = 64
-const CELL  = 10
+const CELL = 9
 
 let img, offscreen
-let imgChange
+let imgFull
 
-function preload (){
+function preload() {
+  myFont = loadFont('PxGrotesk/PxGrotesk-Screen.otf')
+}
 
-	  myFont = loadFont('PxGrotesk/PxGrotesk-Screen.otf');
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  offscreen = createGraphics(NUM_X, NUM_Y)
+  fail_sound = createAudio('fail.mp3')
+  tada_sound = createAudio('tada.mp3')
+  //img = loadImage("imgs_"+Math.floor(random(7))+".jpg")
+
+  //rajouter innerhtml
+  setupNiveau()
+}
+
+function setupNiveau() {
+  console.log(level)
+  img = loadImage(data[level].image)
+  imgFull = loadImage(data[level].image)
 
 }
 
-function setup(){
-	createCanvas(windowWidth, windowHeight)
-  	offscreen = createGraphics(NUM_X, NUM_Y)
-		//img = loadImage("imgs_"+Math.floor(random(7))+".jpg")
 
-		//rajouter innerhtml
-	setupNiveau()
+
+
+function draw() {
+  background(0)
+
+
+  // Resize proportionel et centree de l’image (de la webcam)
+  // sur le "offscreen"
+  const ch = NUM_Y
+  const cw = Math.floor(img.width / img.height * ch)
+  offscreen.image(img, (NUM_X - cw) / 2, 0, cw, ch) //chgm taille
+
+
+  // Preview de l'image:
+  image(offscreen, 0, 0)
+
+  // HACK:
+  // .get() ne marche pas avec "p5.img" (bug de P5JS?)
+  // on est obligé de acceder directement le tableau des pixels[]
+  offscreen.loadPixels()
+
+  // Il faut considerer aussi la densité des pixels
+  const d = offscreen.pixelDensity()
+  const wd = offscreen.width * d * d
+
+
+  // Offset de la matrice
+  const ox = (width - NUM_X * CELL) / 2
+  const oy = (height - NUM_Y * CELL) / 2
+
+  //const sx = Math.round(map(Math.sin(frameCount * 0.0051), -1, 1, 0, 5))
+  const sx = Math.round(map(frameCount * 0.9, 0, width, 0, 6))
+  //const sy = Math.round(map(Math.sin(frameCount * 0.0063), -1, 1, 0, 6))
+  const sy = Math.round(map(frameCount * 0.9, 0, height, 0, 6))
+
+  if (frameCount > 1200) {
+    frameCount = 1200
+  }
+
+  // numero de subdivisons: 1, 2, 4, 8, 16, 32, 64...
+  const subdivisions_x = Math.pow(2, sx)
+  const subdivisions_y = Math.pow(2, sy)
+
+  const cell_w = NUM_X * CELL / subdivisions_x
+  const cell_h = NUM_Y * CELL / subdivisions_y
+
+
+  if (mouseX < width / 2) {
+    cursor('hotdog2.png')
+  } else {
+    cursor('cat2.png')
+  }
+
+  // Affichage final
+  noStroke()
+
+
+  for (let j = 0; j < subdivisions_y; j++) {
+    for (let i = 0; i < subdivisions_x; i++) {
+      const x = i * cell_w + ox
+      const y = j * cell_h + oy
+
+      let xnormal = i / subdivisions_x
+      let ynormal = j / subdivisions_y
+      xnormal = xnormal * wd / 2
+      ynormal = ynormal * offscreen.height
+
+
+      const si = Math.floor(i * NUM_X / subdivisions_x) //si =numx
+      //const si = i*NUM_X//si =numx //lol ca marche pas
+      const sj = Math.floor(j * NUM_Y / subdivisions_y)
+      //const sj = j // lol jarrive pas
+      const offs = (si * d + sj * wd) * 4 // non mirror
+      //const offs = (xnormal + ynormal * wd ) *4
+
+      const r = offscreen.pixels[offs]
+      const g = offscreen.pixels[offs + 1]
+      const b = offscreen.pixels[offs + 2]
+      fill(r, g, b)
+      //ellipse(x,y,cell_w-1, cell_h-1)
+      rect(x, y, cell_w + 1, cell_h + 1)
+      //triangle(x,y,x+cell_w,y+cell_h,x+cell_w,y)
+    }
+  }
+  fill(255)
+  textSize(30)
+  textFont(myFont);
+  text(data[level].mot, width / 2 - 500, height / 2)
+  text(data[level].mot2, width / 2 + 400, height / 2)
+  textSize(60)
+  text(score, width / 2 - 20, height - 50)
+
+
+  rf()
+	txtrep()
 }
-function setupNiveau(){
-	console.log(level)
-	img= loadImage(data[level].image)
-	// document.getElementById('Achoice').innerHTML = data[level].mot
-	// document.getElementById('Bchoice').innerHTML = data[level].mot2
 
-}
-
-
-function draw(){
-  	background(0)
-
-
-  	// Resize proportionel et centree de l’image (de la webcam)
-  	// sur le "offscreen"
-  	const ch = NUM_Y
-  	const cw = Math.floor(img.width / img.height * ch)
-  	offscreen.image(img, (NUM_X-cw)/2, 0, cw, ch) //chgm taille
-
-
-		// Preview de l'image:
-		image(offscreen, 0, 0)
-
-  	// HACK:
-  	// .get() ne marche pas avec "p5.img" (bug de P5JS?)
-  	// on est obligé de acceder directement le tableau des pixels[]
-  	offscreen.loadPixels()
-
-  	// Il faut considerer aussi la densité des pixels
-  	const d  = offscreen.pixelDensity()
-  	const wd = offscreen.width * d * d
-
-
-  	// Offset de la matrice
-		const ox = (width - NUM_X * CELL) / 2
-		const oy = (height - NUM_Y * CELL) / 2
-
-		//const sx = Math.round(map(Math.sin(frameCount * 0.0051), -1, 1, 0, 5))
-		const sx = Math.round(map(frameCount*0.9, 0, width, 0, 6))
-		//const sy = Math.round(map(Math.sin(frameCount * 0.0063), -1, 1, 0, 6))
-		const sy = Math.round(map(frameCount*0.9, 0, height, 0, 6))
-
-		if (frameCount > 1200){
-			frameCount=1200
-
-		}
-		console.log(frameCount)
-		// numero de subdivisons: 1, 2, 4, 8, 16, 32, 64...
-		const subdivisions_x = Math.pow(2, sx)
-		const subdivisions_y = Math.pow(2, sy)
-
-		const cell_w = NUM_X * CELL / subdivisions_x
-		const cell_h = NUM_Y * CELL / subdivisions_y
-
-
-	// Affichage final
-	noStroke()
-
-
-	for (let j=0; j<subdivisions_y; j++) {
-			for (let i=0; i<subdivisions_x; i++) {
-					const x = i * cell_w + ox
-					const y = j * cell_h + oy
-
-					let xnormal = i / subdivisions_x
-					let ynormal = j /subdivisions_y
-					xnormal = xnormal * wd/2
-					ynormal = ynormal * offscreen.height
-
-
-					const si = Math.floor(i * NUM_X / subdivisions_x)//si =numx
-					//const si = i*NUM_X//si =numx //lol ca marche pas
-					const sj = Math.floor(j * NUM_Y / subdivisions_y)
-					//const sj = j // lol jarrive pas
-	  			const offs = (si * d + sj * wd) * 4 // non mirror
-					//const offs = (xnormal + ynormal * wd ) *4
-
-	  			const r = offscreen.pixels[offs    ]
-	  			const g = offscreen.pixels[offs + 1]
-	  			const b = offscreen.pixels[offs + 2]
-	  			fill(r, g, b)
-					//ellipse(x,y,cell_w-1, cell_h-1)
-	  			rect(x, y, cell_w+1, cell_h+1)
-					//triangle(x,y,x+cell_w,y+cell_h,x+cell_w,y)
-  		}
-	}
-	fill(255)
-	textSize(30)
-	textFont(myFont);
-	text(data[level].mot, 100, height/2)
-	text(data[level].mot2, width-300, height/2)
-	text(score,width/2,height-50)
-}
 function mouseClicked() {
 
-frameCount = 1
+  frameCount = 1
+
+  num_click++
+
+  if (num_click % 2 == 0) {
+
+    if (mouseX < windowWidth / 2) {
+      console.log("gauche")
+      if (data[level].reponse == 1) {
+        console.log("right")
+        score++
+      } else {
+        console.log("false")
+      }
+
+    } else {
+      console.log("droite")
+      if (data[level].reponse == 2) {
+        console.log("right")
+        score++
+      } else {
+        console.log("false")
+      }
+    }
+
+    level++
+    if (level >= data.length) {
+      level = 0
+    }
+
+    setupNiveau()
+
+  } else {
+			if (mouseX < windowWidth / 2) {
+			    if (data[level].reponse == 1) {
+			      tada_sound.play()
+			    } else {
+			      fail_sound.play()
+			    }
+			}else{
+				if (data[level].reponse == 2) {
+					tada_sound.play()
+				} else {
+					fail_sound.play()
+				}
+			}
 
 
-if(mouseX < windowWidth/2){
-	console.log("gauche")
-	 if (data[level].reponse == 1){
-		  console.log("right")
-			score++
 
-	 }else{
-		 console.log("false")
+    console.log('click', num_click)
 
-	 }
+  }
+}
 
-}else{
-	console.log("droite")
-	if(data[level].reponse == 2){
-		console.log("right")
-		score++
-	}else{
-	 console.log("false")
+function rf() {
+  if (num_click % 2 == 1) {
+    translate(-576 / 2, -576 / 2)
+    image(imgFull, width / 2, height / 2, 577, 577)
+  } else {
 
+  }
+}
 
- 	}
+function txtrep(){
+
+	fill(255,0,0)
+		if (num_click % 2 == 1) {
+
+						if (data[level].reponse == 1) {
+							text('YEAH', width / 2 - 20, height - 50)
+						} else {
+
+						}
+						if (data[level].reponse == 2) {
+							text('FAIL', width / 2 + 20, height + 50)
+						} else {
+
+						}
+
+		}else{
+
+		}
 
 }
-console.log("bim",score)
-level ++
-if (level >= data.length){
-	level = 0
-}
 
 
-setupNiveau()
-
-//img = loadImage("imgs_"+Math.floor(random(7))+".jpg")
-
-}
-function windowResized(){
-	resizeCanvas(windowWidth, windowHeight)
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
 }
